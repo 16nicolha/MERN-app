55bd05a9a7bb6a3ba63b4f844837b2c5a09562d9
16nicolha <100052466+16nicolha@users.noreply.github.com>
2024-10-07 11:55:21 -0400

    created database with mongoDB, trying to use mongoose to connect to database

---
+++ config/connectToDb.js
@@ -0,0 +1,12 @@
+//load env variables
+if (process.env.NODE_ENV != "production"){
+    require("dotenv").config();
+    }
+
+const mongoose = require('mongoose');
+
+async function connectToDb(){
+    await mongoose.connect(process.env.DB_URL);
+}
+
+module.exports = connectToDb;
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-
+//load env variables
+if (process.env.NODE_ENV != "production"){
+require("dotenv").config();
+}
 
 //import dependencies
-const express = require('express')
+const express = require("express");
+const connectToDb = require("./config/connectToDb");
 
-const app = express()
+//create an express app
+const app = express();
+
+//Connect to database
+connectToDb();
 
 //routing
 app.get('/', (req, res) => {
     res.json({hello: "world"})
-})
+});
 
 //start our server
-app.listen(3000);
+app.listen(process.env.PORT);
