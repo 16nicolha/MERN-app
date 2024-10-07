d20f010eea132c4ad82fc40ca543bad6be1b5ade
16nicolha <100052466+16nicolha@users.noreply.github.com>
2024-10-07 12:38:07 -0400

    using postman to build and use API

--- config/connectToDb.js
+++ config/connectToDb.js
@@ -6,7 +6,13 @@ if (process.env.NODE_ENV != "production"){
 const mongoose = require('mongoose');
 
 async function connectToDb(){
-    await mongoose.connect(process.env.DB_URL);
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        console.log("Connected to database")
+    } catch(err){
+        console.log(err);
+    }
+
 }
 
 module.exports = connectToDb;
--- server.js
+++ server.js
@@ -6,17 +6,34 @@ require("dotenv").config();
 //import dependencies
 const express = require("express");
 const connectToDb = require("./config/connectToDb");
+const Note = require ("./models/note")
 
 //create an express app
 const app = express();
+
+//configure express app
+app.use(express.json());
 
 //Connect to database
 connectToDb();
 
 //routing
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.json({hello: "world"})
 });
+
+app.post('/notes', async (req, res) => {
+    //Get the sent in data off request body
+   const title =  req.body.title;
+   const body = req.body.body;
+    //Create a note with it
+    const note = await Note.create({
+        title: title,
+        body: body,
+    });
+    //respond with the new note
+    res.json({ note: note });
+})
 
 //start our server
 app.listen(process.env.PORT);
