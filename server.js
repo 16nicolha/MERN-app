b39744eac20d24019ca79158329b2ec58d7b2e77
16nicolha <100052466+16nicolha@users.noreply.github.com>
2024-10-07 11:08:32 -0400

    Started process of Creating express backend by installing npm packages of express,mongoose,dotenv, and nodemon. Imported dependencies,added routing,and started server

---
+++ server.js
@@ -0,0 +1,12 @@
+//import dependencies
+const express = require('express')
+
+const app = express()
+
+//routing
+app.get('/', (req, res) => {
+    res.json({hello: "world"})
+})
+
+//start our server
+app.listen(3000);
